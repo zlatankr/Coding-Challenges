9aebaec57f81f0195e2ea806499968778307ad28
zlatankr <zlatankr@gmail.com>
2016-12-28 10:25:20 -0800

    finish number 25

---
+++ Project Euler 25.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 10:01:30 2016
+
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+def number_25(x):
+    fibo = [1, 1]
+    length = len(str(fibo[len(fibo)-1]))
+    while length < x:
+        fibo.append(fibo[len(fibo)-1]+fibo[len(fibo)-2])
+        length = len(str(fibo[len(fibo)-1]))
+    return len(fibo)-1
